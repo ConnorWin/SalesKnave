3497dfa521819948ba8d3dc7ae650827c87bae6a
Sarah Sankovitch <Teddy Sterne>
2019-11-24 09:44:48 -0500

    Add uniform map creation

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,9 @@
-import { Display } from "rot-js/lib/index";
+import { Display, Map } from "rot-js/lib/index";
 
 export class Game {
   private display: Display;
   private gameSize: { width: number; height: number };
+  private map: { [key: string]: string | string[] } = {};
 
   constructor() {
     this.gameSize = { width: 75, height: 25 };
@@ -13,5 +14,57 @@ export class Game {
       fontSize: 20
     });
     document.body.appendChild(this.display.getContainer());
+    this._generateMap();
+  }
+
+  private _generateMap() {
+    const digger = new Map.Uniform(
+      this.gameSize.width,
+      this.gameSize.height,
+      {}
+    );
+    const freeCells = [];
+
+    digger.create((x, y, value) => {
+      if (value) {
+        return;
+      }
+
+      const key = this.key(x, y);
+      this.map[key] = ".";
+      freeCells.push(key);
+    });
+
+    this._drawWholeMap();
+  }
+
+  private _drawWholeMap() {
+    for (var key in this.map) {
+      const [x, y] = this.fromKey(key);
+      this.display.draw(x, y, this.map[key], undefined, undefined);
+      this.drawWalls(x, y);
+    }
+  }
+
+  private drawWalls(x: number, y: number) {
+    for (let dx = x - 1; dx <= x + 1; dx++) {
+      for (let dy = y - 1; dy <= y + 1; dy++) {
+        if (!this.map[this.key(dx, dy)]) {
+          this.display.draw(dx, dy, "#", undefined, undefined);
+        }
+      }
+    }
+  }
+
+  private key(x: number, y: number) {
+    return `${x},${y}`;
+  }
+
+  private fromKey(key: string): [number, number] {
+    const parts = key.split(",");
+    const x = parseInt(parts[0]);
+    const y = parseInt(parts[1]);
+
+    return [x, y];
   }
 }
