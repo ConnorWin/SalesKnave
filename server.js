326f10fd3fc3a087c7f556598ea10cde5e882369
Sarah Sankovitch <Teddy Sterne>
2019-11-24 09:07:05 -0500

    Add prettier

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use('/', express.static(__dirname+'/dist'));
+app.use("/", express.static(__dirname + "/dist"));
 
 app.listen(process.env.port || 3000);
 
-console.log('Running at Port 3000');
+console.log("Running at Port 3000");
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-entry: './src/app.ts',
-module: {
-    rules:[{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-    }]
-},
-devServer: {
-    port: '3000',
+  entry: "./src/app.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    port: "3000",
     // Change it if other port needs to be used
     hot: true,
     // enable HMR on the server
@@ -19,23 +21,23 @@ devServer: {
     quiet: false,
     compress: true,
     // minimize the output to terminal.
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, "dist"),
     // match the output path
-    publicPath: '/'
+    publicPath: "/"
     // match the output `publicPath`
-},
-resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-},
-output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-},
-mode: 'development',
-plugins: [
-  new HtmlWebpackPlugin({
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
       title: "Sales Knave",
       template: "index.html"
-  })
-]
+    })
+  ]
 };
