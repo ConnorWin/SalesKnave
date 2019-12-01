0c8fd6b4a557913f689ae900c36169041316f62f
Connor Winnestaffer <cwinnest@gmail.com>
2019-12-01 11:38:28 -0500

    sales phrases

---
+++ src/boss-fight/data.ts
@@ -0,0 +1,39 @@
+export const logicalPitches = [
+  "The highest trampoline bounce ever recorded launched a man 22 feet in the air. We can get you to 23.",
+  "Trampolines are much safer to jump on than cars or couches.",
+  "Statistically, trampolines are safer than most people think.",
+  "Most trampoline injuries are caused to to improper setup from improper parents.",
+  "Our tramplines used springs or cable depending on your preference.",
+  "Enclosing out trampolines in a net will provide your users with an inescapable cocoon of saftely.",
+  "NASA says using our trampolines for 10 minues is the same as walking for 30 with no ligament stress.",
+  "Our trampolines are recomened for children in pediatric therapy.",
+  "The Center for Disease Control and Prevention says our trampolines provide many of the same benefits as yoga.",
+  "Professional trampolinists never bounce barefoot. Thats why I'll throw in our protective footware for every model you buy."
+];
+
+export const feelingPitches = [
+  "The USA is really good are the trampoline olympics. If your users are American, you owe it to therm to buy these trampolines.",
+  "When a child is not on a trampoline, they're doing something else which could be bad.",
+  "Have you ever seen a sad person on a trampoline?",
+  "Buying one trampoline will save six dolphins...well...it certain won't hurt them and I don't think you want to hurt dolphins.",
+  "*Beg*",
+  "*Cry softly*",
+  "Our trampolines bring smiles to everyone's faces.",
+  "There's no trampoline supplier better than ours.",
+  "Some say bouncing is the window to the soul.",
+  "Our customers say they safest bouncing on one of our trampolines."
+];
+
+export const agressionPitches = [
+  "What? You're too good for our trampolines?",
+  "All our competators are complete garbage compared to us.",
+  "This reduced price that I’m offering you today is only available until the end of the week.",
+  "What can I do to change your mind?",
+  "If I remove the spare parts package does is that a better price point for you?",
+  "You, me, a 10,000 trampoline deal. What do you say?",
+  "You want to use slides at your theme park? No, no trampoliiines, thats where its at these days.",
+  "If you're not satisfied with your first bounce, you can send it all back.",
+  "That's the offer. Bounce it or leave it.",
+  "What can I do to get you leaping and bouncing on our latest model today?",
+  "You can bounce with the other guys or you can bounce in style!"
+];
